f1a43df1ca4907c6a8a29e5a6a13bd36d62fe5ff
Divyansh <divyansh.yadav@wipro.com>
2020-06-13 20:57:31 +0530

    initial commit

---
+++ src/components/Sidebar/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Sidebar() {
+    return (
+        <div>
+            <ul>
+                <li>Bubble sort</li>
+            </ul>
+        </div>
+    )
+}

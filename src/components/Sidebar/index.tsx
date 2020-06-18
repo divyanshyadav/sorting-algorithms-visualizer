699e160e5cd1e76f3548c4e485b025f7196abfb6
Divyansh <divyansh.yadav@wipro.com>
2020-06-18 23:20:08 +0530

    update sidebar

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,17 +1,24 @@
 import React from 'react'
 import { css } from 'emotion'
 
+const links = [
+    { id: "1", label: 'Bubble sort' },
+]
 
 export default function Sidebar() {
 
     const className = css`
         color: hotpink;
+        background-color: lightgrey;
+        max-width: 300px;
+        height: calc(100vh - 40px);
+        padding: 20px;
     `
     
     return (
         <div className={className}>
             <ul>
-                <li>Bubble sort</li>
+                {links.map(link => <li key={link.id}>{link.label}</li>)}
             </ul>
         </div>
     )

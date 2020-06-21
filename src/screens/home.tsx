e950d732937ce060aeef969aa8c585722300f888
Divyansh <divyansh.yadav@wipro.com>
2020-06-21 14:20:09 +0530

    animate insertion sort

--- src/screens/home.tsx
+++ src/screens/home.tsx
@@ -6,11 +6,29 @@ import Column from '../components/column'
 import SortVisualizer from '../components/sort-visualizer'
 import Controls from '../components/controls'
 import { randomNumbersArray } from '../utils/random'
+import insertionSort from '../utils/sorting-algorithms/insertion-sort'
+
+const wait = (ms:number) => new Promise(res => setTimeout(res, ms))
 
 export default function Home() {
+    const [frame, setFrame] = React.useState<number[]>([])
     const style = css`
         height: 100vh;
     `
+    React.useEffect(() => {
+        const array = randomNumbersArray({ length: 50 })
+        const frameArrays: number[][] = insertionSort(array)
+
+        const animate = async () => {
+            for (let i = 0; i < frameArrays.length; i++) {
+                setFrame(frameArrays[i])
+                await wait(10)
+            }
+        }
+
+        animate()
+    }, [])
+
     return (
         <div className={style}>
             <Row>
@@ -18,7 +36,7 @@ export default function Home() {
                     <Sidebar />
                 </Column>
                 <Column grow={5}>
-                    <SortVisualizer data={randomNumbersArray({ length: 100 })} />
+                    <SortVisualizer data={frame} />
                     <Controls />
                 </Column>
             </Row>
--- src/utils/sorting-algorithms/insertion-sort.ts
+++ src/utils/sorting-algorithms/insertion-sort.ts
@@ -1,13 +1,16 @@
+function swap(array: Array<any>, indexA: number, indexB: number) {
+    const temp = array[indexA]
+    array[indexA] = array[indexB]
+    array[indexB] = temp
+}
+
 export default function insertionSort(array: Array<number>) {
     const results = [array.slice()]
 
     for (let i = 1; i < array.length; i++) {
         for (let j = i; j > 0; j--) {
             if (array[j] < array[j - 1]) {
-                const temp = array[j - 1]
-                array[j - 1] = array[j]
-                array[j] = temp
-
+                swap(array, j, j - 1)
                 results.push(array.slice())
             }
         }
